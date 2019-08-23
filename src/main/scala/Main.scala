b8aec0f55ebcb76c01f821d0e264900f95a54b5e
Gr8z <gr8_boi52@yahoo.com>
2019-08-23 22:40:21 +0500

    List -> Seq

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,8 @@
 package org.mq.frogsandtoads
 
 object Main {
-  def legalMoves(board: List[Int]): List[List[Int]] = {
-    var moves = List[List[Int]]()
+  def legalMoves(board: Seq[Int]): Seq[Seq[Int]] = {
+    var moves = Seq[Seq[Int]]()
     for ((piece, pos) <- board.zipWithIndex) {
       val jumpmove = pos + (piece * 2)
       val move = pos + (piece)
@@ -10,16 +10,16 @@ object Main {
         if (!((jumpmove < 0) || (jumpmove >= board.size))) {
           if (board(jumpmove) == 0) {
             var t = board
-            t = t.patch(pos, List(0), 1)
-            t = t.patch(jumpmove, List(piece), 1)
+            t = t.patch(pos, Seq(0), 1)
+            t = t.patch(jumpmove, Seq(piece), 1)
             moves = moves :+ t
           }
         }
         if (!((move < 0) || (move >= board.size))) {
           if (board(move) == 0) {
             var t = board
-            t = t.patch(pos, List(0), 1)
-            t = t.patch(move, List(piece), 1)
+            t = t.patch(pos, Seq(0), 1)
+            t = t.patch(move, Seq(piece), 1)
             moves = moves :+ t
           }
         }
@@ -29,17 +29,17 @@ object Main {
   }
 
   def evalAll(
-      current: List[List[List[Int]]],
-      target: List[Int]
-  ): List[List[List[Int]]] = {
-    var next = List[List[List[Int]]]()
+      current: Seq[Seq[Seq[Int]]],
+      target: Seq[Int]
+  ): Seq[Seq[Seq[Int]]] = {
+    var next = Seq[Seq[Seq[Int]]]()
     for (a <- current) {
       val n = legalMoves(a.last)
       for (q <- n) {
         var t = a
         t = t :+ q
         if (q == target) {
-          return List(t)
+          return Seq(t)
         }
         next = next :+ t
       }
@@ -47,16 +47,16 @@ object Main {
     return next
   }
 
-  def solve(start: List[Int]): List[List[List[Int]]] = {
-    var temp = List(List(start))
+  def solve(start: Seq[Int]): Seq[Seq[Int]] = {
+    var temp = Seq(Seq(start))
     val end = start.reverse
     while (temp.last.last != end) {
       temp = evalAll(temp, end)
     }
-    return temp
+    return temp.last
   }
 
   def main(args: Array[String]) {
-    println(solve(List(1, 1, 1, 0, -1, -1, -1)))
+    println(solve(Seq(1, 1, 1, 0, -1, -1, -1)))
   }
 }
